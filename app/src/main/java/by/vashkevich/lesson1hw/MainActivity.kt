cf9b98b80b13d19f92332e6a7baf806fe2b9dc56
artemVash <p.l.a.y@mail.ru>
2021-04-06 22:27:48 +0300

    Задание 1

---
+++ app/src/main/java/by/vashkevich/lesson1hw/MainActivity.kt
@@ -0,0 +1,11 @@
+package by.vashkevich.lesson1hw
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
