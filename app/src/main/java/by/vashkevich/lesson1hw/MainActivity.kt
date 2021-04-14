5b84f3a95112e9df24009c0623a09220d5f1d4fc
artemVash <p.l.a.y@mail.ru>
2021-04-14 10:46:07 +0300

    Merge

--- app/src/main/java/by/vashkevich/lesson1hw/MainActivity.kt
+++ app/src/main/java/by/vashkevich/lesson1hw/MainActivity.kt
@@ -1,11 +1,21 @@
 package by.vashkevich.lesson1hw
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonTextView1 = findViewById<TextView>(R.id.tv_dz1)
+        val buttonTextView2 = findViewById<TextView>(R.id.tv_dz2)
+
+        buttonTextView1.setOnClickListener {
+            val intent = Intent(this,HomeWork1Activity::class.java)
+            startActivity(intent)
+        }
     }
 }
